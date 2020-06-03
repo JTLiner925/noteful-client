5b588172d50b0f472b152d965ff4b8496037edae
JTLiner925 <[jtliner925@gmail.com]>
2020-06-03 08:49:34 -0500

    working on now.json

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1fa93908e65e6c901318b1170cae3fa5.js"
+  "/precache-manifest.4189437ac183303683c669ce02e8b46d.js"
 );
 
 workbox.clientsClaim();
